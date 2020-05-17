d788d97ba52ce79ca820b91af1da498ef77dbdc8
ricardomoura <ricardo.souza.moura@gmail.com>
2020-05-16 21:47:02 -0300

    Initial Commit

---
+++ buildazure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buildazure
+//
+//  Created by Ricardo Moura on 15/05/20.
+//  Copyright © 2020 Ricardo Moura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
